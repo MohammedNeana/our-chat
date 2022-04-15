4538f0cb4c0c5988644145184a19985a12f6cdb5
unknown <mohammedneana@gmail.com>
2022-04-15 07:12:26 +0200

    RMssg

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,11 +8,13 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import TextBox from '../TextBox/TextBox'
 import RecivedMessage from '../RecivedMessage/RecivedMessage'
-import { Scrollbars } from 'react-custom-scrollbars-2';
+import ReactDOM from 'react-dom';
+
 
 export default function Home() {
     const [spinner, setSpinner] = useState(true);
     const [message, setMessage] = useState([])
+    let isSucc = false;
 
     async function startSSE() {
         let eventSource = new EventSource("http://localhost:4545/ours/sse");
@@ -47,16 +49,21 @@ export default function Home() {
         axios.post('http://localhost:4545/ours/mssg', mssg).then(res => {
             console.log("are we here ?");
             if (res.status === 200) {
+                isSucc = true;
+
+                console.log(isSucc);
                 console.log(res.data.mssg);
                 console.log(mssg.mssg);
+                // const el = <RecivedMessage out={res.data.mssg} />
+                // const root = ReactDOM.createRoot(document.getElementById('root'));
+                // root.render(el);
+                
             }
         })
     }
     async function retriveMssg() {
         axios.get('http://localhost:4545/ours/mssg', mssg).then(res => {
             if (res.status === 200) {
-                // console.log(res.data[0].mssg);
-                // console.log(res.data);
                 setMessage(res.data)
                 setSpinner(false)
             }
@@ -75,7 +82,7 @@ export default function Home() {
             <div className="container-fluid pt-5">
                 <div className="row mx-2 pt-5">
                     <div className="col-lg-8">
-                    <div className="msgTitle d-flex w-100 justify-content-between">
+                            <div className="msgTitle d-flex w-100 justify-content-between">
                             <h2 className='text-center border-bottom pb-lg-2 w-100 pb-3'>Message Group</h2>
                             <button className="navbar-toggler navbar-dark bg-dark mb-2 p-2 mb-2 ms-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                                 <span className='text-white'>Users</span>
@@ -83,8 +90,8 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="col-lg-8 scroll-box mssg-height">
-                        <RecivedMessage msgg={message} />
-                        {/* <SendMessage /> */}
+                            {isSucc ? <RecivedMessage out={mssg.mssg} /> : false}
+                            <RecivedMessage msgg={message} />
                         <SendMessage />
                     </div>
                     <div id={style.activeUsers} className="col-lg-4 ps-5">
